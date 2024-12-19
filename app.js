ee4fb73792e9c551fca67b1a872e40b4316a0d96
Avinash Powar <powaravinash06@gmail.com>
2024-12-19 14:28:16 +0530

    add laying the fiundation basic

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-import React from "react";
+import React, { createElement } from "react";
 import ReactDom from "react-dom/client";
+
+const heading = React.createElement("h1", {}, "Namste Avinash");
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(heading);
