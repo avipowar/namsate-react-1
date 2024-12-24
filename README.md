a5aefa3e22a619015a642ed701899c3d9af16f9e
Avinash Powar <powaravinash06@gmail.com>
2024-12-24 19:00:30 +0530

    add the project filr

---
+++ README.md
@@ -0,0 +1,20 @@
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Matching Algorithm - written in c++
+- Cashing - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support Older Browser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Build AND Prod Builds
+  git
---
+++ app.js
@@ -0,0 +1,73 @@
+import React, { createElement } from "react";
+import ReactDom from "react-dom/client";
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/TopPicks2024/104985400B.png"
+      />
+      <h4> Hotel Qaswa Hills </h4>
+      <h4>4.4</h4>
+      <h4>30-35 mins</h4>
+      <h4> Biryani,North Indian </h4>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk50Ut-wJKwbca3BTPssDUl_fqnsEE_D2tcw&s"
+        />
+      </div>
+      <div className="nav-items">
+        <ul className="nav-items-list">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
