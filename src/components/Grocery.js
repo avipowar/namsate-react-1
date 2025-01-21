681dbcfb1b2a7f1df7a200d0f6a5f9724aeba8da
Avinash Powar <powaravinash06@gmail.com>
2025-01-21 12:27:09 +0530

    add some changes

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,7 +1,17 @@
 const Grocery = () => {
   return (
-    <div>
-      <h1>Its Big Grocery App</h1>
+    <div className="grocery-container">
+      <h1 className="grocery-heading">Welcome to Big Grocery App</h1>
+      <p className="grocery-description">
+        Explore a variety of fresh groceries at your fingertips. Get everything
+        you need with just a click!
+      </p>
+      <div className="grocery-buttons">
+        <button className="grocery-btn grocery-btn-green">
+          Start Shopping
+        </button>
+        <button className="grocery-btn grocery-btn-blue">Explore Offers</button>
+      </div>
     </div>
   );
 };
